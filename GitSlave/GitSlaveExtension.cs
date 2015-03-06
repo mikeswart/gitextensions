21a4a0aa61651cf3deac5a58d0456a90fd384c4b
Mike Swart <mike.swart@gmail.com>
2015-03-06 11:45:01 -0600

    Added GitSlave extension

---
+++ GitSlave/GitSlaveExtension.cs
@@ -0,0 +1,19 @@
+﻿using GitSlave.Internal;
+using GitUIPluginInterfaces;
+
+namespace GitSlave
+{
+    public class GitSlaveExtension : GitPluginBase, IGitPluginForRepository
+    {
+        public override string Description
+        {
+            get { return "GitSlave Extension"; }
+        }
+
+        public override bool Execute(GitUIBaseEventArgs gitUiCommands)
+        {
+            return new BranchPromptWindow().ShowDialog() ?? false;
+            //gitUiCommands.GitUICommands.StartCommandLineProcessDialog(gitUiCommands.OwnerForm, "gits branch")
+        }
+    }
+}
---
+++ GitSlave/Internal/BranchPromptWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace GitSlave.Internal
+{
+    /// <summary>
+    /// Interaction logic for BranchPromptWindow.xaml
+    /// </summary>
+    public partial class BranchPromptWindow : Window
+    {
+        public BranchPromptWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+    }
+}
